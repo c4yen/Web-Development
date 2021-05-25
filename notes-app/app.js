71282a9055f54a9ca3d05e7e05a7cc39fbbaa8c3
c4yen <jyen.studio@gmail.com>
2021-05-25 18:07:42 -0400

    Solved the appending data to file system challenge

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,19 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
+//fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
+
+/** 
+** Challenge: Append a message to notes.txt
+** 
+** 1. Use appendFileSync to append to the file
+** 2. Run the script
+** 3. Check your work by opening the file and viewing the appended text
+*/
+
+try{
+    var data = 'data to append'
+    fs.appendFileSync('notes.txt', data);
+    console.log('The ' + data + ' was appended to file!');
+}catch (err){
+    console.err("Failed to append data to file");
+}
