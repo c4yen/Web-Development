1c5d678f3c808788126d9624780a7827124eae81
c4yen <jyen.studio@gmail.com>
2021-05-25 18:50:34 -0400

    Importing npm Modules, validator module

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,18 +1,9 @@
-// const add = require('./utils.js')
-// const sum = add(4, -4)
-// console.log(sum)
-
-/**
- * Challenge: Define and use a function in a new file
- * 1. Create a new file called notes.js
- * 2. Create getNotes function that returns "Your notes..."
- * 3. Export getNotes function
- * 4. From app.js, load in and call the function printing message to console 
- */
-
+const validator = require('validator')
 const notes = require('./notes.js')
 
 const msg = notes()
-
 console.log(msg);
+
+console.log(validator.isURL('https://example.com'))
+
 
