03caf88039406ec0dcd50fac7be2ac4ed262b529
c4yen <jyen.studio@gmail.com>
2021-05-25 18:59:44 -0400

    learning to use chalk module

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,19 @@
-const validator = require('validator')
 const notes = require('./notes.js')
 
 const msg = notes()
 console.log(msg);
 
-console.log(validator.isURL('https://example.com'))
+/**
+ * Challenge: Use the chalk library in your project
+ * 1. Install chalk
+ * 2. Load chalk into app.js
+ * 3. Use it to print the string "Success!" to the console in green
+ * 4. Test your work
+ * 
+ * Bonus: use docs to mess around with other styles. Make text bold and inversed.
+ */
 
+const chalk = require('chalk')
+console.log(chalk.green('hello'))
+console.log(chalk.bold.inverse('bonus: bold and inversed'))
 
