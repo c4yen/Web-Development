f34a9c653b3ce049ff8882842c7803e408c37dd9
c4yen <jyen.studio@gmail.com>
2021-05-26 09:15:33 -0400

    Notes app, adding notes function added

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,19 +1,72 @@
 const notes = require('./notes.js')
+const chalk = require('chalk')
+const yargs = require('yargs')
+const { describe, demandOption } = require('yargs')
 
-const msg = notes()
-console.log(msg);
+//Customize yargs version
+yargs.version('1.1.0')
+
 
 /**
- * Challenge: Use the chalk library in your project
- * 1. Install chalk
- * 2. Load chalk into app.js
- * 3. Use it to print the string "Success!" to the console in green
- * 4. Test your work
- * 
- * Bonus: use docs to mess around with other styles. Make text bold and inversed.
+ * Challenge: Add an option to yargs
+ * 1. Setup a body option for the add command
+ * 2. Config a description, make it requried, and for it to be a string
+ * 3. Log the body value in the handler function
+ * 4. Test your work!
  */
 
-const chalk = require('chalk')
-console.log(chalk.green('hello'))
-console.log(chalk.bold.inverse('bonus: bold and inversed'))
+//add, remove, read, list
 
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder:{
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Title: ', argv.title)
+        console.log('Body: ', argv.body)
+    }
+})
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing the note')
+    }
+})
+
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'List the notes',
+    handler: function(){
+        console.log('Lising the notes')
+    }
+})
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note',
+    handler: function(){
+        console.log('Reading a note')
+    }
+})
+
+
+
+
+yargs.parse()
