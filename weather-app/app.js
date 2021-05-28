cde98ee13f07b5a81a24222a120eb3b61e547dec
c4yen <jyen.studio@gmail.com>
2021-05-28 12:06:39 -0400

    callback abstraction for geocode function

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,30 +1,22 @@
 const request = require('postman-request')
-
+const geocode = require('./utils/geocode')
 // const access_key = 'fb7696779b87133f3b9471c959109ba2'
 // const query = '44.389355,-79.690331'
 // const url = 'http://api.weatherstack.com/current?access_key=' + access_key + '&query=' + query + "&units=m"
 
 // request ({url: url, json: true}, (error, response) => {
-//     //console.log(response.body.current)
-//     console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees outside. It feels like " + response.body.current.feelslike + " degrees outside.")
+//     if (error){
+//         console.log('Unable to connect to weather service!')
+//     }else if(response.body.error){
+//         console.log('Unable to find location:' + response.body.error)
+//     }else{
+//         console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees outside. It feels like " + response.body.current.feelslike + " degrees outside.")
+//     }
 // })
 
 
-/**
- * Geocoding
- * Address -> Lat/Long -> Weather
- * Goal: Print Lat/Long for Los Angeles
- * 1. Fire off a new request to the URL explored in browser
- * 2. Have the request module parse it as JSON
- * 3. Print both the latitude and longitude to the terminal
- * 4. Test your work!
- */
-const mapBoxAccessKey = "pk.eyJ1Ijoic2hpdmFuZCIsImEiOiJja3A3N3QyeXIwYTRhMnpuNjV0N2M3anYyIn0.FhMOLcf7dnJbe7fKATrVRA"
-//const mapBoxQuery = 
-const mapBoxUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=" + mapBoxAccessKey
 
-request({url: mapBoxUrl, json: true}, (error, response) => {
-    const longitude = response.body.features[0].center[0]
-    const latitude = response.body.features[0].center[1]
-    console.log(longitude+ "," + latitude)
+geocode('New York', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,20 @@
+const request = require('postman-request')
+const geocode = (address, callback) => {
+    const mapBoxAccessKey = "pk.eyJ1Ijoic2hpdmFuZCIsImEiOiJja3A3N3QyeXIwYTRhMnpuNjV0N2M3anYyIn0.FhMOLcf7dnJbe7fKATrVRA"
+    const mapBoxUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+ encodeURIComponent(address) +".json?access_token=" + mapBoxAccessKey
+    request({url: mapBoxUrl, json:true}, (error, response) =>{
+        if (error){
+            callback('Unable to connect to location services!', undefined)
+        }else if (response.body.features.length === 0){
+            callback('Unable to find location. Try another search.', undefined)
+        }else{
+            callback(undefined, {
+                longitude: response.body.features[0].center[0],
+                latitude: response.body.features[0].center[1],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
