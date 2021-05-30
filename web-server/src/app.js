4d21fe3deac2c8e55c651f16dfc1e0d58a0d6f15
c4yen <jyen.studio@gmail.com>
2021-05-29 22:10:28 -0400

    created front end for weather app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,60 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-app.get('', (req, res) => {
-    res.send('Hello Express!')
+//define paths for Express Config
+const publicDirectory = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve
+app.use(express.static(publicDirectory))
+
+app.get('', (req, res) =>{
+    res.render('index', {
+        title: 'Home Page',
+        name: 'Joseph Yen'
+    })
 })
 
-app.get('/help', (req, res) => {
-    res.send('Help Page')
+app.get('/about', (req, res) =>{
+    res.render('about', {
+        title: 'About Page',
+        name: 'Joseph Yen'
+    })
 })
-
-app.get('/about', (req, res) => {
-    res.send('About Page')
+app.get('/help', (req, res) =>{
+    res.render('about', {
+        title: 'Help Page', 
+        name: 'Joseph Yen'
+    })
 })
 
 app.get('/weather', (req, res) => {
     res.send('Weather Page')
+})
+
+app.get('/help/*', (req, res) =>{
+    res.render('404', {
+        title: '404', 
+        name: 'Joseph Yen',
+        errorMsg: 'Help article Not Found'
+    })
+})
+
+app.get('*', (req, res) =>{
+    res.render('404', {
+        title: '404', 
+        name: 'Joseph Yen',
+        errorMsg: 'Page Not Found'
+    })
 })
 
 app.listen(3000, () =>{
