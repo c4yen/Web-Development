49cbc36099132a8020663dbf65d95504b4287311
c4yen <jyen.studio@gmail.com>
2021-06-14 14:43:54 -0400

    data validation with validator part 1

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,26 +1,67 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useUnifiedTopology: true
 })
 
 const User = mongoose.model('User', {
     name:{
-        type: String
+        type: String,
+        require: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        require: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if (!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value){
+            if (value < 0){
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
 const me = new User({
-    name: 'Joseph',
-    age: 27
+    name: '   Andrew   ',
+    email: 'mike@email.com       '
 })
 
 me.save().then(() => {
     console.log(me)
 }).catch((error) => {
-    console.log('Error!', error)
+    console.log(error)
 })
+
+// const Task = mongoose.model('Tasks', {
+//     description:{
+//         type: String
+//     },
+//     completed: {
+//         type: Boolean
+//     }
+// })
+
+// const task = new Task({
+//     description: 'Learn the mongoose Library',
+//     completed: false
+// })
+
+// task.save().then(() => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log(error)
+// })
