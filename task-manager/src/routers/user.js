47b19de0b2257974b6d782b6512031d6194bd118
c4yen <jyen.studio@gmail.com>
2021-06-22 10:47:03 -0400

    added image resize and formatting

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router
-
+const sharp = require('sharp')
 const multer = require('multer')
 
 
@@ -101,7 +101,8 @@ const upload = multer({
 
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async(req, res) => {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 }, (error, req, res, next) => {
@@ -122,9 +123,9 @@ router.get('/users/:id/avatar', async(req, res) => {
             throw new Error()
         }
 
-        res.set('Content-Type', 'image/jpg')
+        res.set('Content-Type', 'image/png')
         res.send(user.avatar)
-        
+
     }catch (e){
         res.status(404).send()
     }
