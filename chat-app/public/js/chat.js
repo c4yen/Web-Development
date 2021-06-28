0fab505fb60e3d9a0884fab96adfdfe13d027f01
c4yen <jyen.studio@gmail.com>
2021-06-28 17:03:51 -0400

    chat app, added send location

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,9 +1,31 @@
 const socket = io()
 
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated!', count)
+socket.on('message', (message) =>{
+    console.log(message)
 })
 
-document.querySelector('#increment').addEventListener('click', () => {
-    socket.emit('increment')
+document.querySelector('#messaage-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message)
 })
+
+document.querySelector('#send-location').addEventListener('click', (e) => {
+    if (!navigator.geolocation){
+        return alert('Geolocation is not supported by your browser.')
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+})
+
+// socket.on('countUpdated', (count) => {
+//     console.log('The count has been updated!', count)
+// })
+
+// document.querySelector('#increment').addEventListener('click', () => {
+//     socket.emit('increment')
+// })
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -12,17 +12,31 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
 
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('countUpdated', count)
-    socket.on('increment', () => {
-        count++
-        //socket.emit('countUpdated', count)
-        io.emit('countUpdated', count)
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
     })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+
+    // socket.emit('countUpdated', count)
+    // socket.on('increment', () => {
+    //     count++
+    //     //socket.emit('countUpdated', count)
+    //     io.emit('countUpdated', count)
+    // })
 })
 
 server.listen(port, ()=> {
