6d714f11563b47bd60a612d8a0de9371acb02389
c4yen <jyen.studio@gmail.com>
2021-07-13 08:22:46 -0400

    Rendering user list in chat room

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -10,9 +10,14 @@ const $messages = document.querySelector('#messages')
 //Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector('#location-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 //options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+const autoscroll = () => {
+    
+}
 
 socket.on('message', (message) =>{
     console.log(message)
@@ -32,6 +37,14 @@ socket.on('locationMessage', (location) =>{
         createdAt: moment(location.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
 })
 
 $messageForm.addEventListener('submit', (e) => {
